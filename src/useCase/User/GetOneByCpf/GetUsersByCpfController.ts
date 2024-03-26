b28d38bf4376daf97f56a30d9dbcf1b1cd5a9140
josuerx12 <josueaze12@gmail.com>
2024-03-26 16:33:31 -0300

    fix: Routes fixes

--- src/useCase/User/GetOneByCpf/GetUsersByCpfController.ts
+++ src/useCase/User/GetOneByCpf/GetUsersByCpfController.ts
@@ -5,7 +5,7 @@ export class GetUsersByCpfController {
   constructor(private GetUsersByCpfUseCase: GetUsersByCpfUseCase) {}
 
   handle = async (req: Request, res: Response) => {
-    const payload = await this.GetUsersByCpfUseCase.execute(req.body.cpf);
+    const payload = await this.GetUsersByCpfUseCase.execute(req.params.cpf);
 
     return res.send({ payload });
   };
--- src/useCase/User/GetOneByCpf/GetUsersByCpfUseCase.ts
+++ src/useCase/User/GetOneByCpf/GetUsersByCpfUseCase.ts
@@ -2,8 +2,9 @@ import User from "../../../entities/User";
 
 export class GetUsersByCpfUseCase {
   async execute(cpf: string) {
+    console.log(cpf);
     const users = await User.findOne({
-      cpf: cpf.replace(".", "").replace("-", "").trim(),
+      cpf: String(cpf).replace(".", "").replace("-", "").trim(),
     }).select("-password");
 
     return users;
