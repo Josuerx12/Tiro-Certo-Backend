d71948159344a9888ef7c2f43de9a8774f4825e4
josuerx12 <josueaze12@gmail.com>
2024-03-26 09:56:58 -0300

    feat: security to firebase credentials added.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,13 +1,25 @@
 import admin, { ServiceAccount } from "firebase-admin";
 import { config } from "dotenv";
 
-import serviceAccount from "./tirocerto.json";
-
 config();
+
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key.replace(/\\n/g, "\n"),
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as ServiceAccount),
-  storageBucket: "tirocerto-e86bc.appspot.com",
+  storageBucket: process.env.BUCKET,
 });
 
 export { admin };
